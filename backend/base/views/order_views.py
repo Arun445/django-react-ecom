cccf6772f0e2bad086677547e25044a3f32313ad
arunas445 <skittlessxit@gmail.com>
2021-05-15 20:21:26 +0300

    Added functionality to create users. Added signals and serializers

---
+++ backend/base/views/order_views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from ..products import products
+from ..models import *
+from ..serializers import ProductSerializer, UserSerializer, UserSerializerWithToken
--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
+from rest_framework import status
 
-from .products import products
-from .models import *
-from .serializers import ProductSerializer
+from ..models import *
+from ..serializers import ProductSerializer, UserSerializer
 
-# Create your views here.
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/products'
-    ]
-    return Response(routes)
-    
 @api_view(['GET'])
 def getProducts(request):
     products = Product.objects.all() 
