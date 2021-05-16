3a4e232d39a554cec107a289f58e62af20b2c6a3
arunas445 <skittlessxit@gmail.com>
2021-05-17 01:40:32 +0300

    Finishing the checkout functionality. Adding paypal pay method

--- backend/base/views/order_views.py
+++ backend/base/views/order_views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
@@ -56,3 +57,29 @@ def addOrderItems(request):
 
         serializer = OrderSerializer(order,many=False)
         return Response(serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+    user = request.user
+    try:
+        order=Order.objects.get(_id=pk)
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail':'notauthorized to view this order'},status = status.http.status.HTTP_400_BAD_REQUEST)
+    except:
+        return Response({'detail':'Order does not exist'}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def updateOrderToPaid(request,pk):
+    order = Order.objects.get(_id=pk)
+
+    order.isPaid=True
+    order.paidAt = datetime.now()
+
+    order.save()
+
+    return Response('Order was paid')
